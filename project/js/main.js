af8554532819df28fd511143d1f6e9b9302b9836
shangria <shangrialena@gmail,com>
2020-01-03 01:00:25 +0200

    Add files for github pages

---
+++ project/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: false,
+        infinite: true,
+        centerMode: false
+    });
+});
+
